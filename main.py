da2ed9daa1f6623929aaacc68969261533c9dec8
heifl00 <130314992+heifl00@users.noreply.github.com>
2023-04-14 12:55:02 +0200

    formate code
    line width was adjusted

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import taupunktberechnung
 import onboard_T
 import wifi
 
-##########################################################################################################
+########################################################################
 
 # global variables
 telegram_api_key = "5876295566:AAEIo9IDt8u4TkzVWcfIPLB-w3951lbVde4"
@@ -17,7 +17,7 @@ led = Pin("LED", Pin.OUT)
 
 print_auto_values = False
 
-##########################################################################################################
+########################################################################
 # send a message without a message from the user
 def message_without_call(userchatid, text):
     bot.send(userchatid, text)
@@ -25,20 +25,30 @@ def message_without_call(userchatid, text):
 # welcome message
 def welcome_message():
     userchatid = secrets.userchatid
-    welcome = ("<pre>Hallo! Hier ist der Bot-TauPi! Hier findest du die Uebersicht über alle Befehle zur Steuerung von TauPi: </pre> \n"
+    welcome = ("<pre>Hallo! Hier ist der Bot-TauPi! Hier findest </pre>"
+               "<pre>du die Uebersicht über alle Befehle zur"
+               "<pre>Steuerung von TauPi: </pre> \n"
                "<pre>----------------------------\n"
                "   --Einmalige Abfragen-- \n"
                "----------------------------</pre>\n"
-               "<pre> - zeigt beim benutzen eines Befehls einmalig die gewuenschten Werte des Schimmelwaechters an.</pre> \n"
-               "/bme <pre> = zeigt alle Werte des BME280 an</pre> \n"
-               "/temp <pre> = zeigt die Oberflaechentemperatur an</pre> \n"
-               "/humi <pre> = zeigt die rel. Luftfeuchtigkeit an</pre> \n"
+               "<pre> - zeigt beim benutzen eines Befehls </pre>"
+               "<pre>einmalig die gewuenschten Werte des </pre>" 
+               "<pre>Schimmelwaechters an.</pre> \n"
+               "/bme <pre> = zeigt alle Werte des BME280 an</pre>\n"
+               "/temp <pre> = zeigt die </pre> \n"
+               "<pre>Oberflaechentemperatur an</pre>\n"
+               "/humi <pre> = zeigt die rel. </pre>"
+               "<pre>Luftfeuchtigkeit an</pre>\n"
                "<pre>----------------------------\n"
                " --Automatische Abfragen--\n"
                "----------------------------</pre>\n"
-               "<pre> - zeigt bei eingeschalteter Funktion kontinuierlich alle Werte des Schimmelwaechters an</pre> \n"
-               "/auto_on <pre> = schaltet die automatischen Ausgabe an</pre> \n"
-               "/auto_off <pre> = schaltet die automatischen Ausgabe ab</pre> \n")
+               "<pre> - zeigt bei eingeschalteter Funktion </pre>"
+               "<pre>kontinuierlich alle Werte des </pre>"
+               "<pre>Schimmelwaechters an</pre> \n"
+               "/auto_on <pre> = schaltet die automatischen </pre>"
+               "<pre>Ausgabe an</pre> \n"
+               "/auto_off <pre> = schaltet die automatischen </pre> \n"
+               "<pre>Ausgabe ab</pre> \n")
     message_without_call(userchatid, welcome)
     
 # auto send message when bme is not available
@@ -60,24 +70,30 @@ def SchiPi_Warnung():
     userchatid = secrets.userchatid
     
     if result[4] == True:
-        text = ('Es besteht keine Schimmelgefahr die Ergebnisse sehen wie folgt aus: ' + '\n')
+        text = ('Es besteht keine Schimmelgefahr die Ergebnisse' +
+                'sehen wie folgt aus: ' + '\n')
     else:
-        text = ('Es besteht Schimmelgefahr die Ergebnisse sehen wie folgt aus:' + '\n')
+        text = ('Es besteht Schimmelgefahr die Ergebnisse sehen' +
+                'wie folgt aus:' + '\n')
 
     result_text = ("Oberflächentemperatur: {:<10.2f} &#176;C\n"
                    "rel. Luftfeuchte:      {:<10.1f} %\n"
                    "Raumtemperatur:        {:<10.5f} &#176;C\n"
-                   "Taupunkttemperatur:    {:<10.5f} &#176;C\n").format(result[2], result[1], result[0], result[3])
+                   "Taupunkttemperatur:    {:<10.5f} &#176;C\n"
+                  ).format(result[2], result[1], result[0], result[3])
     # style the text to code block text
     code_block_text = "<pre>" + text + result_text + "</pre>"
     message_without_call(userchatid, code_block_text)
     
-##########################################################################################################
+########################################################################
 # send a message as an answer
 # Telegram default callback
 def get_message(message):
-    text = ('<pre>TauPi kann mit dieser Nachricht nichts anfangen. Beim klicken auf Menu findest du alle vorhandenen Befehle.</pre>')
-    bot.reply_to(message['message']['chat']['id'], text, message['message']['message_id'])
+    text = ('<pre>TauPi kann mit dieser Nachricht nichts </pre>'
+            '<pre>anfangen. Beim klicken auf Menu findest </pre>'
+            '<pre>du alle vorhandenen Befehle.</pre>')
+    bot.reply_to(message['message']['chat']['id'], text, 
+                 message['message']['message_id'])
 
 # send PONG text as ping reply
 def reply_ping(message):
@@ -86,20 +102,30 @@ def reply_ping(message):
 
 # send an overview of all commands
 def overview(message):
-    welcome = ("<pre>Hier findest du die Uebersicht über alle Befehle zur Steuerung von TauPi: </pre> \n"
+    welcome = ("<pre>Hier findest </pre>"
+               "<pre>du die Uebersicht über alle Befehle zur"
+               "<pre>Steuerung von TauPi: </pre> \n"
                "<pre>----------------------------\n"
                "   --Einmalige Abfragen-- \n"
                "----------------------------</pre>\n"
-               "<pre> - zeigt beim benutzen eines Befehls einmalig die gewuenschten Werte des Schimmelwaechters an.</pre> \n"
-               "/bme <pre> = zeigt alle Werte des BME280 an</pre> \n"
-               "/temp <pre> = zeigt die Oberflaechentemperatur an</pre> \n"
-               "/humi <pre> = zeigt die rel. Luftfeuchtigkeit an</pre> \n"
+               "<pre> - zeigt beim benutzen eines Befehls </pre>"
+               "<pre>einmalig die gewuenschten Werte des </pre>" 
+               "<pre>Schimmelwaechters an.</pre> \n"
+               "/bme <pre> = zeigt alle Werte des BME280 an</pre>\n"
+               "/temp <pre> = zeigt die </pre> \n"
+               "<pre>Oberflaechentemperatur an</pre>\n"
+               "/humi <pre> = zeigt die rel. </pre>"
+               "<pre>Luftfeuchtigkeit an</pre>\n"
                "<pre>----------------------------\n"
                " --Automatische Abfragen--\n"
                "----------------------------</pre>\n"
-               "<pre> - zeigt bei eingeschalteter Funktion kontinuierlich alle Werte des Schimmelwaechters an</pre> \n"
-               "/auto_on <pre> = schaltet die automatischen Ausgabe an</pre> \n"
-               "/auto_off <pre> = schaltet die automatischen Ausgabe ab</pre> \n")
+               "<pre> - zeigt bei eingeschalteter Funktion </pre>"
+               "<pre>kontinuierlich alle Werte des </pre>"
+               "<pre>Schimmelwaechters an</pre> \n"
+               "/auto_on <pre> = schaltet die automatischen </pre>"
+               "<pre>Ausgabe an</pre> \n"
+               "/auto_off <pre> = schaltet die automatischen </pre> \n"
+               "<pre>Ausgabe ab</pre> \n")
     bot.send(message['message']['chat']['id'], welcome)
 
 # send all values as answer
@@ -107,36 +133,46 @@ def bme280_all_values(message):
     temp = bme280_data.bme280_temp()
     humi = bme280_data.bme280_humi()
     text = ("<pre>Oberflächentemperatur:   {:<5.2f} &#176;C\n"
-            "rel. Luftfeuchtigkeit:   {:<5.2f} %</pre>").format(temp, humi)
+            "rel. Luftfeuchtigkeit:   {:<5.2f} %</pre>"
+           ).format(temp, humi)
     bot.send(message['message']['chat']['id'], text)
 
 # send temp value as answer
 def bme280_temp(message):
     temp = bme280_data.bme280_temp()
-    text = ("<pre>Oberflächentemperatur:   {:<5.2f} &#176;C</pre>").format(temp)
+    text = ("<pre>Oberflächentemperatur:   {:<5.2f} &#176;C</pre>"
+           ).format(temp)
     bot.send(message['message']['chat']['id'], text)
 
 # send humi value as answer
 def bme280_humi(message):
     humi = bme280_data.bme280_humi()
-    text = ("<pre>rel. Luftfeuchtigkeit:   {:<5.2f} %</pre>").format(humi)
+    text = ("<pre>rel. Luftfeuchtigkeit:   {:<5.2f} %</pre>"
+           ).format(humi)
     bot.send(message['message']['chat']['id'], text)
     
 # auto send values on (print_auto_values = True)
 def auto_values_on(message):
-    bot.send(message['message']['chat']['id'], '<pre>Es werden ab jetzt immer automatisch die aktuellen Daten angezeigt.' + '\n' +
-    'Möchtest du das beenden benutze den Befehl </pre>/auto_off.')
+    bot.send(message['message']['chat']['id'], 
+             '<pre>Es werden ab jetzt immer automatisch ' + 
+             'die aktuellen Daten angezeigt.' + '\n' +
+             'Möchtest du das beenden ' + 
+             'benutze den Befehl </pre>/auto_off.')
     global print_auto_values
     print_auto_values = True    
 
 # auto send values off (print_auto_values = False)
 def auto_values_off(message):
-    bot.send(message['message']['chat']['id'], '<pre>Es werden ab jetzt keine aktuellen Daten mehr automatisch angezeigt.' + '\n' +
-    'Möchtest du automatisch dir die aktuellen Daten anzeigen lassen, benutze den Befehl </pre>/auto_on.')
+    bot.send(message['message']['chat']['id'], 
+             '<pre>Es werden ab jetzt keine aktuellen ' + 
+             'Daten mehr automatisch angezeigt.' + '\n' +
+             'Möchtest du automatisch dir die ' + 
+             'aktuellen Daten anzeigen lassen, ' + 
+             'benutze den Befehl </pre>/auto_on.')
     global print_auto_values
     print_auto_values = False
 
-##########################################################################################################
+########################################################################
 
 # WLAN
 wifi.connect()
@@ -149,21 +185,28 @@ print('Bot ist da!')
 welcome_message()
 
 # register handler-functions
-# Register the 'reply_ping' function to be called when '/ping' command is received
+# Register the 'reply_ping' function to be called 
+# when '/ping' command is received
 bot.register('/ping', reply_ping)
-# Register the 'overview' function to be called when '/overview' command is received
+# Register the 'overview' function to be called
+# when '/overview' command is received
 bot.register('/overview', overview)
 
-# Register the 'bme280_all_values' function to be called when '/bme' command is received
+# Register the 'bme280_all_values' function to be called
+# when '/bme' command is received
 bot.register('/bme', bme280_all_values)
-# Register the 'bme280_temp' function to be called when '/temp' command is received
+# Register the 'bme280_temp' function to be called
+# when '/temp' command is received
 bot.register('/temp', bme280_temp)
-# Register the 'bme280_humi' function to be called when '/humi' command is received
+# Register the 'bme280_humi' function to be called
+# when '/humi' command is received
 bot.register('/humi', bme280_humi)
 
-# Register the 'auto_values_on' function to be called when '/auto_on' command is received
+# Register the 'auto_values_on' function to be called
+# when '/auto_on' command is received
 bot.register('/auto_on', auto_values_on)
-# Register the 'auto_values_off' function to be called when '/auto_off' command is received
+# Register the 'auto_values_off' function to be called
+# when '/auto_off' command is received
 bot.register('/auto_off', auto_values_off)
 
 # Register the 'get_message' function to be called
@@ -188,7 +231,8 @@ while True:
     print('Bot parse message.')
     part = bot.read_once()
          
-    if print_auto_values: # ist das gleiche wie: print_auto_values == True:
+    if print_auto_values: 
+        # print_auto_values == True:
         SchiPi_Warnung()
         
     time.sleep(2)
